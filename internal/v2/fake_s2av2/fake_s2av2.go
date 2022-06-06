ef4c8d5a023f6c165fec000d8e96c84a5b6ccd1f
Riya Mehta <riyamehta@google.com>
2022-06-06 21:56:29 +0000

    rev 4

--- internal/v2/fake_s2av2/fake_s2av2.go
+++ internal/v2/fake_s2av2/fake_s2av2.go
@@ -2,9 +2,44 @@ package fake_s2av2
 
 import (
 	"crypto/tls"
-	// import s2a.proto?
+	s2av2pb "github.com/google/s2a-go/internal/proto/v2/s2a_go_proto"
+	commonpb "github.com/google/s2a-go/internal/proto/v2/common_go_proto"
 )
 
-func GetTlsConfiguration() () {
+type Server struct {
+	pb.UnimplementedS2AServiceServer
+}
+
+func (s *Server) GetTlsConfiguration(ctx context.Context, req *s2av2pb.GetTlsConfigurationReq) (*s2av2pb.GetTlsConfigurationResp, error) {
+	if s2av2.GetConnectionSide() == commonpb.ConnectionSide_CONNECTION_SIDE_CLIENT {
+		return &s2av2pb.GetTlsConfigurationResp {
+			ClientTlsConfiguration: {
+				certificate_chain: []string{
+					{/*TODO(rmehta19)*/},
+				},
+				min_tls_version: commonpb.TLSVersion_TLS_VERSION_1_3,
+				max_tls_version: commonpb.TLSVersion_TLS_VERSION_1_3,
+				handshake_ciphersuites: commonpb.HandshakeCiphersuite_HANDSHAKE_CIPHERSUITE_UNSPECIFIED,
+				record_ciphersuites: commonpb.RecordCiphersuite_RECORD_CIPHERSUITE_UNSPECIFIED,
+			},
+		}, nil
+	}
+	else if s2av2.GetConnectionSide() == commonpb.ConnectionSide_CONNECTION_SIDE_SERVER {
+		return &s2av2pb.GetTlsConfigurationResp {
+			certificate_chain: []string{
+				{/*TODO(rmehta19)*/},
+			},
+			min_tls_version: commonpb.TLSVersion_TLS_VERSION_1_3,
+			max_tls_version: commonpb.TLSVersion_TLS_VERSION_1_3,
+			handshake_ciphersuites: commonpb.HandshakeCiphersuite_HANDSHAKE_CIPHERSUITE_UNSPECIFIED,
+			record_ciphersuites: commonpb.RecordCiphersuite_RECORD_CIPHERSUITE_UNSPECIFIED,
+			tls_resumption_enabled: false,
+			request_client_certificate: s2av2pb.GetTlsConfigurationResp_ServerTlsConfiguration_REQUEST_AND_VERIFY,
+			max_overhead_of_ticket_aead: 0,
+		}, nil
+	}
+	else {
+		return nil, nil
+	}
 }
 
--- internal/v2/tls_config_store/tls_config_store.go
+++ internal/v2/tls_config_store/tls_config_store.go
@@ -5,13 +5,20 @@ import (
 	"log"
 	"crypto/tls"
 	"crypto/x509"
+	"context"
+	"flag"
 	"github.com/google/s2a-go/internal/v2/cert_verifier"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	_ "embed"
-	pb "github.com/google/s2a-go/internal/proto/v2/s2a_go_proto"
+	s2av2pb "github.com/google/s2a-go/internal/proto/v2/s2a_go_proto"
+	commonpb "github.com/google/s2a-go/internal/proto/v2/common_go_proto"
+	fakes2av2 ""
 )
 
 var (
+	fakes2av2Addr = flag.String("fakes2av2_Addr", "0.0.0.0:8080", "Fake S2Av2 service address.")
 	//go:embed example_cert_key/client_cert.pem
 	clientCert []byte
 	//go:embed example_cert_key/server_cert.pem
@@ -24,9 +31,34 @@ var (
 
 // GetTlsConfigurationForClient returns a tls.Config instance for use by a client application.
 func GetTlsConfigurationForClient(serverHostname string) *tls.Config {
-	// TODO(rmehta19): Call S2Av2 for certificate.
+	// TODO(rmehta19): Reuse this connection per session across all libraries. Perhaps move the connection setup to s2av2.go.
+	// Setup connection to fake S2Av2.
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithReturnConnectionError(),
+		grpc.WithBlock(),
+	}
+	conn, err := grpc.Dial(*fakes2av2Addr, opts...)
+	if err != nil {
+		log.Fatalf("Client Application: failed to connect: %v", err)
+	}
+	defer conn.Close()
+	c := s2av2pb.NewS2AServiceClient(conn)
+	log.Infof("Client Application: connected to: %s", *fakes2av2Addr)
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+	defer cancel()
+	// Call fake S2Av2 for cofig.
+	r, err := fakes2av2.GetTlsConfiguration(ctx, &s2av2pb.GetTlsConfigurationReq{
+		connection_side: commonpb.ConnectionSide_CONNECTION_SIDE_CLIENT,
+		sni: "",
+	})
+	if err != nil {
+		log.Fatalf("Client Application: failed to send GetTlsConfigurationReq: %v", err)
+	}
+	log.Infof("Client Application: recieved GetTlsConfigurationResponse from server")
+
 	// TODO(rmehta19): Call remote signer library for private key.
-	cert, err := tls.X509KeyPair(clientCert, clientKey)
+	cert, err := tls.X509KeyPair(r.certificate_chain[0], clientKey)
 	if err != nil {
 		log.Fatalf("Failed to generate X509KeyPair: %v", err)
 	}
@@ -34,7 +66,6 @@ func GetTlsConfigurationForClient(serverHostname string) *tls.Config {
 	rootCertPool := x509.NewCertPool()
 	rootCertPool.AppendCertsFromPEM(serverCert)
 
-	// TODO(rmehta19): Call S2Av2 for config values.
 	// Create mTLS credentials for client.
 	return &tls.Config {
 		Certificates: []tls.Certificate{cert},
@@ -43,8 +74,8 @@ func GetTlsConfigurationForClient(serverHostname string) *tls.Config {
 		ServerName: serverHostname,
 		InsecureSkipVerify: true,
 		ClientSessionCache: nil,
-		MinVersion: uint16(tls.VersionTLS13),
-		MaxVersion: uint16(tls.VersionTLS13),
+		MinVersion: r.min_tls_version,
+		MaxVersion: r.max_tls_version,
 	}
 }
 
