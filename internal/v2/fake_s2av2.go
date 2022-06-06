8d768f818e6de485d7f6ec3062d2e3604b9f46b4
Riya Mehta <riyamehta@google.com>
2022-06-06 17:44:10 +0000

    rev 2

--- internal/v2/fake_s2av2.go
+++
@@ -1,6 +0,0 @@
-package fake_s2av2
-
-import (
-)
-
-// S2Av2 returns 
---
+++ internal/v2/fake_s2av2/fake_s2av2.go
@@ -0,0 +1,10 @@
+package fake_s2av2
+
+import (
+	"crypto/tls"
+	// import s2a.proto?
+)
+
+func GetTlsConfiguration() () {
+}
+
