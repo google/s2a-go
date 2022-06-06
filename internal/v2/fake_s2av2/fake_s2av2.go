c2ccb7c6fcd2fefa3906583193e377fd4a8e1397
Riya Mehta <riyamehta@google.com>
2022-06-06 23:25:48 +0000

    rev 5

--- internal/v2/fake_s2av2/fake_s2av2.go
+++ internal/v2/fake_s2av2/fake_s2av2.go
@@ -1,45 +1,52 @@
 package fake_s2av2
 
 import (
-	"crypto/tls"
+	"context"
+
 	s2av2pb "github.com/google/s2a-go/internal/proto/v2/s2a_go_proto"
 	commonpb "github.com/google/s2a-go/internal/proto/v2/common_go_proto"
 )
 
 type Server struct {
-	pb.UnimplementedS2AServiceServer
+	s2av2pb.UnimplementedS2AServiceServer
 }
 
 func (s *Server) GetTlsConfiguration(ctx context.Context, req *s2av2pb.GetTlsConfigurationReq) (*s2av2pb.GetTlsConfigurationResp, error) {
-	if s2av2.GetConnectionSide() == commonpb.ConnectionSide_CONNECTION_SIDE_CLIENT {
+	if req.GetConnectionSide() == commonpb.ConnectionSide_CONNECTION_SIDE_CLIENT {
 		return &s2av2pb.GetTlsConfigurationResp {
-			ClientTlsConfiguration: {
-				certificate_chain: []string{
-					{/*TODO(rmehta19)*/},
+			TlsConfiguration: &s2av2pb.GetTlsConfigurationResp_ClientTlsConfiguration_{
+				&s2av2pb.GetTlsConfigurationResp_ClientTlsConfiguration{
+					CertificateChain: []string{
+						/*TODO(rmehta19)*/
+						"",
+					},
+					MinTlsVersion: commonpb.TLSVersion_TLS_VERSION_1_3,
+					MaxTlsVersion: commonpb.TLSVersion_TLS_VERSION_1_3,
+					HandshakeCiphersuites: []commonpb.HandshakeCiphersuite{},
+					RecordCiphersuites: []commonpb.RecordCiphersuite{},
 				},
-				min_tls_version: commonpb.TLSVersion_TLS_VERSION_1_3,
-				max_tls_version: commonpb.TLSVersion_TLS_VERSION_1_3,
-				handshake_ciphersuites: commonpb.HandshakeCiphersuite_HANDSHAKE_CIPHERSUITE_UNSPECIFIED,
-				record_ciphersuites: commonpb.RecordCiphersuite_RECORD_CIPHERSUITE_UNSPECIFIED,
 			},
 		}, nil
-	}
-	else if s2av2.GetConnectionSide() == commonpb.ConnectionSide_CONNECTION_SIDE_SERVER {
+	} else if req.GetConnectionSide() == commonpb.ConnectionSide_CONNECTION_SIDE_SERVER {
 		return &s2av2pb.GetTlsConfigurationResp {
-			certificate_chain: []string{
-				{/*TODO(rmehta19)*/},
+			TlsConfiguration: &s2av2pb.GetTlsConfigurationResp_ServerTlsConfiguration_{
+				&s2av2pb.GetTlsConfigurationResp_ServerTlsConfiguration{
+					CertificateChain: []string{
+						/*TODO(rmehta19)*/
+						"",
+					},
+					MinTlsVersion: commonpb.TLSVersion_TLS_VERSION_1_3,
+					MaxTlsVersion: commonpb.TLSVersion_TLS_VERSION_1_3,
+					HandshakeCiphersuites: []commonpb.HandshakeCiphersuite{},
+					RecordCiphersuites: []commonpb.RecordCiphersuite{},
+					TlsResumptionEnabled: false,
+					RequestClientCertificate: s2av2pb.GetTlsConfigurationResp_ServerTlsConfiguration_REQUEST_AND_VERIFY,
+					MaxOverheadOfTicketAead: 0,
+				},
 			},
-			min_tls_version: commonpb.TLSVersion_TLS_VERSION_1_3,
-			max_tls_version: commonpb.TLSVersion_TLS_VERSION_1_3,
-			handshake_ciphersuites: commonpb.HandshakeCiphersuite_HANDSHAKE_CIPHERSUITE_UNSPECIFIED,
-			record_ciphersuites: commonpb.RecordCiphersuite_RECORD_CIPHERSUITE_UNSPECIFIED,
-			tls_resumption_enabled: false,
-			request_client_certificate: s2av2pb.GetTlsConfigurationResp_ServerTlsConfiguration_REQUEST_AND_VERIFY,
-			max_overhead_of_ticket_aead: 0,
 		}, nil
-	}
-	else {
-		return nil, nil
+	} else {
+		return nil, nil /*TODO(rmehta19): replace with some error message.*/
 	}
 }
 
