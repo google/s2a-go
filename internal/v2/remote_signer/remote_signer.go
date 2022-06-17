d044758c77b0d5b359e93ca16a1c0f10048dcbce
Riya Mehta <riyamehta@google.com>
2022-06-17 22:23:21 +0000

    Remote Signer makes RPC to Fake S2Av2

--- internal/v2/remote_signer/remote_signer.go
+++ internal/v2/remote_signer/remote_signer.go
@@ -2,13 +2,78 @@
 package remotesigner
 
 import (
+	"io"
 	"crypto"
-	"crypto/tls"
+	"crypto/x509"
+	s2av2pb "github.com/google/s2a-go/internal/proto/v2/s2a_go_proto"
+	commonpbv1 "github.com/google/s2a-go/internal/proto/common_go_proto"
 )
+
+// remoteSigner implementes the crypto.Signer interface.
+type remoteSigner struct {
+	leafCert *x509.Certificate
+	cstream s2av2pb.S2AService_SetUpSessionClient
+	localIdentity *commonpbv1.Identity
+}
+
 
 // New returns an instance of RemoteSigner, an implementation of the
 // crypto.Signer interface.
-func New(cert *tls.Certificate) crypto.Signer {
-	// TODO(rmehta19): Implement crypto.Signer interface that calls S2Av2.
-	return cert.PrivateKey.(crypto.Signer)
+func New(leafCert *x509.Certificate, cstream s2av2pb.S2AService_SetUpSessionClient, localIdentity *commonpbv1.Identity) crypto.Signer {
+	return &remoteSigner{leafCert, cstream, localIdentity}
+}
+
+func (s *remoteSigner) Public() crypto.PublicKey {
+	return s.leafCert.PublicKey
+}
+
+func (s *remoteSigner) Sign(rand io.Reader, digest []byte, opts crypto.SignerOpts) (signature []byte, err error) {
+	// Send request to S2Av2 to perform private key operation.
+	if err := s.cstream.Send(&s2av2pb.SessionReq {
+		LocalIdentity: s.localIdentity,
+		AuthenticationMechanisms: []*s2av2pb.AuthenticationMechanism {
+			{
+				// TODO(rmehta19): Populate Authentication Mechanism using tokenmanager.
+				MechanismOneof: &s2av2pb.AuthenticationMechanism_Token{"token"},
+			},
+		},
+		ReqOneof: &s2av2pb.SessionReq_OffloadPrivateKeyOperationReq {
+			&s2av2pb.OffloadPrivateKeyOperationReq {
+				Operation: s2av2pb.OffloadPrivateKeyOperationReq_SIGN,
+				// TODO(rmehta19): Use Signtuare Algorithm from s.leafCert. To do this,
+				// need to create a mapping from x509 Signature Algorithm:
+				// https://pkg.go.dev/crypto/x509#SignatureAlgorithm
+				// to s2av2 signature algorithm: https://github.com/google/s2a-go/blob/
+				// 2eb8a32e71c9747a4e56196460bfd0feafb5189b/internal/proto/v2/
+				// s2a_go_proto/s2a.pb.go#L43
+				SignatureAlgorithm: s2av2pb.SignatureAlgorithm_S2A_SSL_SIGN_RSA_PSS_RSAE_SHA256,
+				InBytes: []byte(digest),
+			},
+		},
+	}); err != nil {
+		return nil, err
+	}
+
+	// Get the response containing config from S2Av2.
+	resp, err := s.cstream.Recv()
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.GetOffloadPrivateKeyOperationResp().GetOutBytes(), nil
+}
+
+// getCert returns the leafCert field in s.
+func (s *remoteSigner) getCert() *x509.Certificate {
+	return s.leafCert
+}
+
+// getStream returns the cstream field in s.
+func (s *remoteSigner) getStream() s2av2pb.S2AService_SetUpSessionClient {
+	return s.cstream
+}
+
+// getLocalIdentity returns the localIdentity field in s.
+func (s *remoteSigner) getLocalIdentity() *commonpbv1.Identity {
+	return s.localIdentity
 }
--- internal/v2/remote_signer/remote_signer_test.go
+++ internal/v2/remote_signer/remote_signer_test.go
@@ -1,28 +1,151 @@
 package remotesigner
 
 import (
+	"net"
+	"log"
+	"sync"
+	"time"
+	"bytes"
+	"context"
 	"testing"
 	"crypto"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/sha256"
 	"crypto/tls"
 	_ "embed"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"github.com/google/s2a-go/internal/v2/fakes2av2"
+	s2av2pb "github.com/google/s2a-go/internal/proto/v2/s2a_go_proto"
+	commonpbv1 "github.com/google/s2a-go/internal/proto/common_go_proto"
 )
 
+const (
+	defaultTimeout = 10.0 * time.Second
+)
+
+func startFakeS2Av2Server(wg *sync.WaitGroup) (stop func(), address string, err error) {
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatalf("failed to listen on address %s: %v", address, err)
+	}
+	address = listener.Addr().String()
+	s := grpc.NewServer()
+	log.Printf("Server: started gRPC fake S2Av2 Server on address: %s", address)
+	s2av2pb.RegisterS2AServiceServer(s, &fakes2av2.Server{})
+	go func() {
+		wg.Done()
+		if err := s.Serve(listener); err != nil {
+			log.Printf("failed to serve: %v", err)
+		}
+	}()
+	return func() { s.Stop()}, address, nil
+}
 var (
 	//go:embed example_cert_key/cert.pem
-	cert []byte
+	clientCertPEM []byte
+	//go:embed example_cert_key/cert.der
+	clientCertDER []byte
 	//go:embed example_cert_key/key.pem
-	key []byte
+	clientKeyPEM []byte
 )
 
-// TestNewSigner runs unit test for New.
-func TestNewSigner(t *testing.T) {
-	// Setup for static test.
-	cert, err := tls.X509KeyPair(cert, key)
+func TestSign(t *testing.T) {
+	// Start up fake S2Av2 server.
+	var wg sync.WaitGroup
+	wg.Add(1)
+	stop, address, err := startFakeS2Av2Server(&wg)
+	wg.Wait()
 	if err != nil {
-		t.Errorf("failed to parse PEM cert and key: %v", err)
+		log.Fatalf("error starting fake S2Av2 Server: %v", err)
 	}
 
-	if got, want := New(&cert), cert.PrivateKey.(crypto.Signer); got != want {
-		t.Errorf("RemoteSigner instance is incorrect. got: %v, want: %v", got, want)
+	// Create stream to S2Av2.
+	opts := []grpc.DialOption {
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithReturnConnectionError(),
+		grpc.WithBlock(),
+	}
+	conn, err := grpc.Dial(address, opts...)
+	if err != nil {
+		log.Fatalf("Client: failed to connect: %v", err)
+	}
+	defer conn.Close()
+	c := s2av2pb.NewS2AServiceClient(conn)
+	log.Printf("Client: connected to: %s", address)
+	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
+	defer cancel()
+
+	// Setup bidrectional streaming session.
+	callOpts := []grpc.CallOption{}
+	cstream, err := c.SetUpSession(ctx, callOpts...)
+	if err != nil  {
+		log.Fatalf("Client: failed to setup bidirectional streaming RPC session: %v", err)
+	}
+	log.Printf("Client: set up bidirectional streaming RPC session.")
+
+	// TODO(rmehta19): convert into a for loop when adding more tests.
+	// Setup data for testing Sign
+	clientTlsCert, err := tls.X509KeyPair(clientCertPEM, clientKeyPEM)
+	if err != nil {
+		log.Fatalf("tls.X509KeyPair failed: %v", err)
+	}
+	clientx509Cert, err := x509.ParseCertificate(clientCertDER)
+	if err != nil {
+		log.Fatalf("failed to parse cert: %v", err)
+	}
+	s := New(clientx509Cert, cstream, &commonpbv1.Identity {
+		IdentityOneof: &commonpbv1.Identity_Hostname {
+			Hostname: "client_hostname",
+		},
+	})
+	testInBytes := []byte("Test data.")
+
+	// TODO(rmehta19): Investigate whether go crypto libraries compute hash.
+	// If so, remove this line, and just pass testInBytes as digest.
+	hsha256 := sha256.Sum256([]byte(testInBytes))
+	signerOpts := crypto.SHA256
+
+	// Test Sign.
+	gotSignedBytes, err := s.Sign(rand.Reader, hsha256[:], signerOpts)
+	if err != nil {
+		t.Errorf("call to remote signer Sign API failed: %v", err)
+	}
+	wantSignedBytes, err := clientTlsCert.PrivateKey.(crypto.Signer).Sign(rand.Reader, hsha256[:], signerOpts)
+	if err != nil {
+		t.Errorf("call to Sign API failed: %v", err)
+	}
+	if !bytes.Equal(gotSignedBytes, wantSignedBytes) {
+		t.Errorf("gotSignedBytes = %v, wantSignedBytes = %v", gotSignedBytes, wantSignedBytes)
+	}
+	stop()
+}
+
+// TestNew runs unit test for New.
+func TestNew(t *testing.T) {
+	// Setup data for testing New.
+	clientx509Cert, err := x509.ParseCertificate(clientCertDER)
+	if err != nil {
+		t.Errorf("failed to parse cert: %v", err)
+	}
+	localIdentity := &commonpbv1.Identity {
+		IdentityOneof: &commonpbv1.Identity_Hostname {
+			Hostname: "client_hostname",
+		},
+	}
+	var cstream s2av2pb.S2AService_SetUpSessionClient
+
+	// Test New.
+	got := New(clientx509Cert, cstream, localIdentity)
+	if v := got.(*remoteSigner).getCert(); v != clientx509Cert {
+		t.Errorf("RemoteSigner leafCert field is incorrect. got: %v, want: %v", v, clientx509Cert)
+	}
+	if v := got.(*remoteSigner).getStream(); v != cstream {
+		t.Errorf("RemoteSigner cstream field is incorrect. got: %v, want: %v", v, cstream)
+	}
+	if v := got.(*remoteSigner).getLocalIdentity(); v != localIdentity {
+		t.Errorf("RemoteSigner hostname field is incorrect. got: %v, want: %v", v, localIdentity)
 	}
 }
