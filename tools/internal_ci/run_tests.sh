b8dec190d983cbf61dad330b2ad59f8e707bca00
Matthew Stevenson <52979934+matthewstevenson88@users.noreply.github.com>
2022-11-17 12:50:11 -0800

    Fix MacOS Kokoro failures on update to Mojave. (#83)
    * Attempt #1 to fix MacOS Kokoro failures.
    
    * Attemp #2
    
    * attempt #3
    
    * attemp #4
    
    * attempt #5
    
    * attempt #6
    
    * attemp #6 or 7 i lost track

--- tools/internal_ci/run_tests.sh
+++ tools/internal_ci/run_tests.sh
@@ -32,7 +32,8 @@ run_tests() {
 
 main() {
   if [[ -n "${KOKORO_ROOT}" ]]; then
-    use_bazel.sh "4.2.1"
+    chmod +x "${KOKORO_GFILE_DIR}/use_bazel.sh"
+    "${KOKORO_GFILE_DIR}/use_bazel.sh" 4.2.1
   fi
   run_tests
 }
