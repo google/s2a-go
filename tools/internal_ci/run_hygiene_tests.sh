9807eaafaed0e106d9e6c7cef11abe05ed086698
Kui Xu <xmenxk@google.com>
2023-09-01 22:16:20 +0000

    update kokoro tests

--- tools/internal_ci/run_hygiene_tests.sh
+++ tools/internal_ci/run_hygiene_tests.sh
@@ -32,7 +32,7 @@ sudo rm -rf /usr/local/go
 case "${PLATFORM}" in
   'linux')
     sudo rm -rf /usr/local/go
-    curl -O https://dl.google.com/go/go1.17.1.linux-amd64.tar.gz
+    curl -O https://dl.google.com/go/go1.19.12.linux-amd64.tar.gz
     tar -xvf go1.17.1.linux-amd64.tar.gz
     sudo mv go /usr/local
     export GOROOT=/usr/local/go
@@ -40,7 +40,7 @@ case "${PLATFORM}" in
     ;;
   'darwin')
     sudo rm -rf /usr/local/go
-    curl -O https://dl.google.com/go/go1.17.1.darwin-amd64.tar.gz
+    curl -O https://dl.google.com/go/go1.19.12.darwin-amd64.tar.gz
     tar -xvf go1.17.1.darwin-amd64.tar.gz
     sudo mv go /usr/local
     export GOROOT=/usr/local/go
